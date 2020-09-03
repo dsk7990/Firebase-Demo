f9b2e7d4495adc94dce3e1545a20d18515b25e40
darshakparmar <darshak.parmar7@gmail.com>
2020-09-04 02:04:18 +0530

    Initial Commit

--- app/src/main/java/com/firebase/test/DiscreteScrollViewOptions.java
+++ app/src/main/java/com/firebase/test/DiscreteScrollViewOptions.java
@@ -27,25 +27,6 @@ public class DiscreteScrollViewOptions {
         instance = new DiscreteScrollViewOptions(context);
     }
 
-//    public static void configureTransitionTime(DiscreteScrollView scrollView) {
-//        final BottomSheetDialog bsd = new BottomSheetDialog(scrollView.getContext());
-//        final TransitionTimeChangeListener timeChangeListener = new TransitionTimeChangeListener(scrollView);
-//        bsd.setContentView(R.layout.dialog_transition_time);
-//        defaultPrefs().registerOnSharedPreferenceChangeListener(timeChangeListener);
-//        bsd.setOnDismissListener(new DialogInterface.OnDismissListener() {
-//            @Override
-//            public void onDismiss(DialogInterface dialog) {
-//                defaultPrefs().unregisterOnSharedPreferenceChangeListener(timeChangeListener);
-//            }
-//        });
-//        bsd.findViewById(R.id.dialog_btn_dismiss).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                bsd.dismiss();
-//            }
-//        });
-//        bsd.show();
-//    }
 
     public static void smoothScrollToUserSelectedPosition(final DiscreteScrollView scrollView, View anchor) {
         PopupMenu popupMenu = new PopupMenu(scrollView.getContext(), anchor);
